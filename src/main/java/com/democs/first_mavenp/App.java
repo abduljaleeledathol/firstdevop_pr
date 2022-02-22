e3cafbf30c1424aee37e527f0b6643dca173b5ef
Abdul Jaleel <jaleelaaa786@gmail.com>
2022-02-22 17:00:34 +0530

    added initial commit

---
+++ src/main/java/com/democs/first_mavenp/App.java
@@ -0,0 +1,13 @@
+package com.democs.first_mavenp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
